05e750ff694b8d5c755d6cfa42100501d6f54211
M11275710 <154969996+M11275710@users.noreply.github.com>
2024-01-31 22:13:26 +0800

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -2,16 +2,40 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from api.chatgpt import ChatGPT
+#from api.chatgpt import ChatGPT
+
+import random
+import json
+import torch
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
 
 import os
-
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
 app = Flask(__name__)
-chatgpt = ChatGPT()
+#chatgpt = ChatGPT()
 
 # domain root
 @app.route('/')
@@ -31,7 +55,6 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -54,9 +77,30 @@ def handle_message(event):
         return
 
     if working_status:
-        chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
-        reply_msg = chatgpt.get_response().replace("AI:", "", 1)
-        chatgpt.add_msg(f"AI:{reply_msg}\n")
+        #chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
+        #reply_msg = chatgpt.get_response().replace("AI:", "", 1)
+        #chatgpt.add_msg(f"AI:{reply_msg}\n")
+        sentence = event.message.text
+
+        sentence = tokenize(sentence)
+        X = bag_of_words(sentence, all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(device)
+
+        output = model(X)
+        _, predicted = torch.max(output, dim=1)
+
+        tag = tags[predicted.item()]
+
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.75:
+            for intent in intents['intents']:
+                if tag == intent["tag"]:
+                    reply_msg = random.choice(intent['responses'])
+        else:
+           reply_msg = 'I do not understand...'
+
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
