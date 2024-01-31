b3a800578040b14e93558affdda33a0eb65f1e29
M11275710 <154969996+M11275710@users.noreply.github.com>
2024-01-31 22:38:11 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,6 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 #from api.chatgpt import ChatGPT
 
 import random
-import simplejson
 import torch
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
