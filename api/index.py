94d344dce95d99e21a93674a59f4afe0342b9698
M11275710 <154969996+M11275710@users.noreply.github.com>
2024-02-04 22:18:14 +0800

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -4,37 +4,13 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 #from api.chatgpt import ChatGPT
 
-import random
-import torch
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
-
+import replicate
 import os
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-with open('intents.json', 'r') as json_data:
-    intents = json.load(json_data)
-
-FILE = "data.pth"
-data = torch.load(FILE)
-
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
-
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
-
 app = Flask(__name__)
-#chatgpt = ChatGPT()
 
 # domain root
 @app.route('/')
@@ -76,29 +52,9 @@ def handle_message(event):
         return
 
     if working_status:
-        #chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
-        #reply_msg = chatgpt.get_response().replace("AI:", "", 1)
-        #chatgpt.add_msg(f"AI:{reply_msg}\n")
-        sentence = event.message.text
-
-        sentence = tokenize(sentence)
-        X = bag_of_words(sentence, all_words)
-        X = X.reshape(1, X.shape[0])
-        X = torch.from_numpy(X).to(device)
-
-        output = model(X)
-        _, predicted = torch.max(output, dim=1)
-
-        tag = tags[predicted.item()]
-
-        probs = torch.softmax(output, dim=1)
-        prob = probs[0][predicted.item()]
-        if prob.item() > 0.75:
-            for intent in intents['intents']:
-                if tag == intent["tag"]:
-                    reply_msg = random.choice(intent['responses'])
-        else:
-           reply_msg = 'I do not understand...'
+        output = replicate.run("meta/codellama-70b-instruct:a279116fe47a0f65701a8817188601e2fe8f4b9e04a518789655ea7b995851bf",
+        input={"prompt": event.message.text, })
+        reply_msg = "".join(output)
 
         line_bot_api.reply_message(
             event.reply_token,
